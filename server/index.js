14d68addf86c0b509a34cabc89d5333f9e498004
Vilius Ramulionis <vilius.ramulionis@gmail.com>
2022-05-30 22:19:41 +0300

    Build update

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import profile from './controller/profile.js'
 import portfolio from './controller/portfolio.js'
 import cookieParser from 'cookie-parser'
 import authenticate from './middleware/authenticate.js'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
  
@@ -15,6 +19,12 @@ app.use( express.urlencoded({
 app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static('uploads'))
+app.use('/', express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
 app.use('/api/users/', users)
 app.use('/api/profiles/', profile)
 app.use('/api/portfolio/', portfolio)
